0010a6edf465150173331faacc368c55b89f6d25
Flames <vibe-coder@flames.com>
2025-11-06 06:17:24 +0000

    Update project prj_lr9zg3td - 2025-11-06T06:17:23.679Z

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar.jsx';
 import Hero from './components/Hero.jsx';
 import CoursesPreview from './components/CoursesPreview.jsx';
+import Highlights from './components/Highlights.jsx';
 import Footer from './components/Footer.jsx';
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <Navbar />
       <main>
         <Hero />
+        <Highlights />
         <CoursesPreview />
       </main>
       <Footer />
